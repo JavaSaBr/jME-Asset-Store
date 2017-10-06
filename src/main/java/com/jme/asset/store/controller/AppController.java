b1ad2e493dfc6424e82d7988ffa755f439b33a64
Denis <denislesheniuk@gmail.com>
2017-10-07 01:38:17 +0300

    added downloadRandom() method

--- src/main/java/com/jme/asset/store/controller/AppController.java
+++ src/main/java/com/jme/asset/store/controller/AppController.java
@@ -3,7 +3,11 @@ package com.jme.asset.store.controller;
 import ch.qos.logback.core.net.SyslogOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -13,9 +17,8 @@ import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.util.Random;
 
 
 @Controller
@@ -76,10 +79,36 @@ public class AppController {
         }
         return new ResponseEntity<String>("File is added! ",HttpStatus.CREATED);
     }
+
+    //      test_post/send_text - принятие большого теста и вывода его в консоль
     @RequestMapping(value = "test_post/send_text", method = RequestMethod.POST)
     public ResponseEntity<?> sendText(@RequestParam("text") String text){
         logger.info(text);
         return new ResponseEntity<Object>("Text is sended",HttpStatus.OK);
+    }
+
+    //      download/random - при вызове метода, генерирует файл с небольшим случайным содержанием и отправлен вызывающей программе.
+    @RequestMapping(path = "/download/random", method = RequestMethod.GET)
+    public ResponseEntity<Resource> downloadRandom() throws IOException {
+        File file = new File("file.txt");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        Random random = new Random(System.currentTimeMillis());
+        try(FileWriter writer = new FileWriter(file)) {
+            for (int i = 0; i <= random.nextInt(10000); i++) {
+            writer.append((char)random.nextInt());
+            }
+        }catch (IOException exc){
+            exc.printStackTrace();
+        }
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
@@ -97,7 +126,6 @@ public class AppController {
     private ResponseEntity<String> handleMissingParams(NullPointerException ex) {
         return new ResponseEntity<String>("Parameter is missing!!!", HttpStatus.CONFLICT);
     }
-
 
     @ExceptionHandler(MissingServletRequestPartException.class)
     private ResponseEntity<String> handleMissingParams(MissingServletRequestPartException ex) {
