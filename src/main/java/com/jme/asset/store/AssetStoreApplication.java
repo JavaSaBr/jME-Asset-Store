510ff0db7d29090b3e6f4b959c649cb883805efa
Denis <denislesheniuk@gmail.com>
2017-10-06 20:42:51 +0300

    refactoring

--- src/main/java/com/jme/asset/store/AssetStoreApplication.java
+++ src/main/java/com/jme/asset/store/AssetStoreApplication.java
@@ -2,24 +2,13 @@ package com.jme.asset.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
+@SpringBootApplication
 @EnableAutoConfiguration
 public class AssetStoreApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
 
-    @RequestMapping(value = "/math/add", method = RequestMethod.GET )
-    @ResponseBody
-    String add(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
-        return "The sum is = " + (first + second);
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AssetStoreApplication.class, args);
