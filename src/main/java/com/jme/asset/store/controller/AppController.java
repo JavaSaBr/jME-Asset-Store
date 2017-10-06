f189367fbb60c910df5e58186a149fdb8218bdbd
Denis <denislesheniuk@gmail.com>
2017-10-06 21:44:03 +0300

    added method handleFileUpload(), folder resource/public to gitignore

--- src/main/java/com/jme/asset/store/controller/AppController.java
+++ src/main/java/com/jme/asset/store/controller/AppController.java
@@ -9,6 +9,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 
 @Controller
@@ -39,18 +44,34 @@ public class AppController {
         return new ResponseEntity<Object>(first * second, HttpStatus.OK);
     }
 
-    //  /pow - возводит первое число в степень второго числа
+    //  math/pow - возводит первое число в степень второго числа
     @RequestMapping(value = "math/pow", method = RequestMethod.GET )
     public ResponseEntity<?>   pov(@RequestParam("first") Double first, @RequestParam("second") Double second) {
         return new ResponseEntity<Object>(Math.pow(first, second), HttpStatus.OK);
     }
 
-    //  /devide - делит первое на второе
+    //  math/devide - делит первое на второе
     @RequestMapping(value = "math/devide", method = RequestMethod.GET )
     public ResponseEntity<?>   devide(@RequestParam("first") Integer first, @RequestParam("second") Integer second) {
         return new ResponseEntity<Object>((double)first / (double)second, HttpStatus.OK);
     }
 
+    //  test_post/uplaod_file - загрузка файла на диск сервера
+    @RequestMapping(value = "test_post/upload_file", method = RequestMethod.POST)
+    public ResponseEntity<?> handleFileUpload(@RequestParam("file")MultipartFile file){
+        File upfile = new File("src/main/resources/public/" + file.getOriginalFilename());
+        if(file.isEmpty()){
+            return new ResponseEntity("File is empty", HttpStatus.CONFLICT);
+        }
+        try (BufferedOutputStream stream =
+                     new BufferedOutputStream(new FileOutputStream(upfile))){
+            byte[] bytes = file.getBytes();
+            stream.write(bytes);
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return new ResponseEntity<String>("File added! ",HttpStatus.CREATED);
+    }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     private ResponseEntity<String> handleMissingParams(MissingServletRequestParameterException ex) {
