28146dc345e31f046388fbc574dd48db235c760f
mishagrodno <mishagrodno@mail.ru>
2017-10-07 14:30:21 +0300

    added check for params

--- src/main/java/com/jme/asset/store/AssetStoreApplication.java
+++ src/main/java/com/jme/asset/store/AssetStoreApplication.java
@@ -2,12 +2,22 @@ package com.jme.asset.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 @EnableAutoConfiguration
 public class AssetStoreApplication {
+
+
 
     @RequestMapping("/")
     @ResponseBody
@@ -15,39 +25,94 @@ public class AssetStoreApplication {
         return "Hello World!";
     }
 
+
     @RequestMapping(value = "/math/add", method = RequestMethod.GET )
     @ResponseBody
-    String add(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
-        return first +  " + " + second + " = "  + (first + second);
+    String add(HttpServletRequest request){
+        Enumeration<String> e =  request.getParameterNames();
+        int size = request.getParameterMap().size();
+        if(size == 1) return "add 1 more param" ;
+        if(size == 0) return "add 2 more params";
+        if(size == 3) return "1 extra param is present";
+        if(size > 3)  return (size-2) + " extra params are present";
+        String f = request.getParameter(e.nextElement());
+        String s = request.getParameter(e.nextElement());
+        int first = Integer.valueOf(f);
+        int second = Integer.valueOf(s);
+        return  first +  " + " + second + " = "  + (first + second);
     }
 
     @RequestMapping(value = "/math/sub", method = RequestMethod.GET )
     @ResponseBody
-    String sub(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+    String sub(HttpServletRequest request){
+        Enumeration<String> e =  request.getParameterNames();
+        int size = request.getParameterMap().size();
+        if(size == 1) return "add 1 more param" ;
+        if(size == 0) return "add 2 more params";
+        if(size == 3) return "1 extra param is present";
+        if(size > 3)  return (size-2) + " extra params are present";
+        String f = request.getParameter(e.nextElement());
+        String s = request.getParameter(e.nextElement());
+        int first = Integer.valueOf(f);
+        int second = Integer.valueOf(s);
         return first +  " - " + second + " = "  + (first - second);
     }
 
     @RequestMapping(value = "/math/mult", method = RequestMethod.GET )
     @ResponseBody
-    String multiply(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+    String multiply(HttpServletRequest request){
+        Enumeration<String> e =  request.getParameterNames();
+        int size = request.getParameterMap().size();
+        if(size == 1) return "add 1 more param" ;
+        if(size == 0) return "add 2 more params";
+        if(size == 3) return "1 extra param is present";
+        if(size > 3)  return (size-2) + " extra params are present";
+        String f = request.getParameter(e.nextElement());
+        String s = request.getParameter(e.nextElement());
+        int first = Integer.valueOf(f);
+        int second = Integer.valueOf(s);
         return first +  " x " + second + " = "  + (first * second);
     }
 
     @RequestMapping(value = "/math/division", method = RequestMethod.GET )
     @ResponseBody
-    String division(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+    String division(HttpServletRequest request){
+        Enumeration<String> e =  request.getParameterNames();
+        int size = request.getParameterMap().size();
+        if(size == 1) return "add 1 more param" ;
+        if(size == 0) return "add 2 more params";
+        if(size == 3) return "1 extra param is present";
+        if(size > 3)  return (size-2) + " extra params are present";
+        String f = request.getParameter(e.nextElement());
+        String s = request.getParameter(e.nextElement());
+        int first = Integer.valueOf(f);
+        int second = Integer.valueOf(s);
         if(second==0) return "Division by zero is impossible!!!";
         return first +  " / " + second + " = "  + ((double)first / (double)second);
     }
 
     @RequestMapping(value = "/math/pow", method = RequestMethod.GET )
     @ResponseBody
-    String pow(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+    String pow(HttpServletRequest request){
+        Enumeration<String> e =  request.getParameterNames();
+        int size = request.getParameterMap().size();
+        if(size == 1) return "add 1 more param" ;
+        if(size == 0) return "add 2 more params";
+        if(size == 3) return "1 extra param is present";
+        if(size > 3)  return (size-2) + " extra params are present";
+        String f = request.getParameter(e.nextElement());
+        String s = request.getParameter(e.nextElement());
+        int first = Integer.valueOf(f);
+        int second = Integer.valueOf(s);
         return first +  " ^ " + second + " = "  + (int)Math.pow((double)first, (double)second);
     }
+
+
 
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AssetStoreApplication.class, args);
     }
+
+
 }
