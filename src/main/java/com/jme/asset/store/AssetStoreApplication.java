fc5f9dc9a2dd6fce4bbfc52432ea1f0effbde74b
mishagrodno <mishagrodno@mail.ru>
2017-10-07 11:47:51 +0300

    added math/

--- src/main/java/com/jme/asset/store/AssetStoreApplication.java
+++ src/main/java/com/jme/asset/store/AssetStoreApplication.java
@@ -18,8 +18,33 @@ public class AssetStoreApplication {
     @RequestMapping(value = "/math/add", method = RequestMethod.GET )
     @ResponseBody
     String add(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
-        return "The sum is = " + (first + second);
+        return first +  " + " + second + " = "  + (first + second);
     }
+
+    @RequestMapping(value = "/math/sub", method = RequestMethod.GET )
+    @ResponseBody
+    String sub(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+        return first +  " - " + second + " = "  + (first - second);
+    }
+
+    @RequestMapping(value = "/math/mult", method = RequestMethod.GET )
+    @ResponseBody
+    String multiply(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+        return first +  " x " + second + " = "  + (first * second);
+    }
+
+    @RequestMapping(value = "/math/division", method = RequestMethod.GET )
+    @ResponseBody
+    String division(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+        return first +  " / " + second + " = "  + ((double)first / (double)second);
+    }
+
+    @RequestMapping(value = "/math/pow", method = RequestMethod.GET )
+    @ResponseBody
+    String pow(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+        return first +  " ^ " + second + " = "  + (int)Math.pow((double)first, (double)second);
+    }
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AssetStoreApplication.class, args);
