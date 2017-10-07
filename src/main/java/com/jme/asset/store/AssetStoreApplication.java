5adb662ffa8ea4c91d3f255efc85f50d3082df63
AlenaSalanevich <aliceoxx@gmail.com>
2017-10-07 12:00:54 +0300

    add new functions

--- src/main/java/com/jme/asset/store/AssetStoreApplication.java
+++ src/main/java/com/jme/asset/store/AssetStoreApplication.java
@@ -18,9 +18,33 @@ public class AssetStoreApplication {
     @RequestMapping(value = "/math/add", method = RequestMethod.GET )
     @ResponseBody
     String add(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
-        return "The sum is = " + (first + second);
+        return "The addition result is = " + (first + second);
     }
+    @RequestMapping(value = "/math/sub", method = RequestMethod.GET )
+    @ResponseBody
+    String sub(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
+        return "The subtraction result is = " + (second - first);
+    }
+    @RequestMapping(value = "/math/mult", method = RequestMethod.GET )
+    @ResponseBody
+    String mult(@RequestParam("first") Integer first, @RequestParam("second") Integer second) {
+        return "The multiplication result is = " + (second * first);
+    }
+    @RequestMapping(value = "/math/div", method = RequestMethod.GET )
+    @ResponseBody
+    String div(@RequestParam("first") Integer first, @RequestParam("second") Integer second) {
+            if (!(second==0))
+                return "The division result is = " + ((double)first/second);
+           return "The division by zero is not allowed!";
 
+        }
+    @RequestMapping(value = "/math/pow", method = RequestMethod.GET )
+    @ResponseBody
+    String pow(@RequestParam("first") Integer first, @RequestParam("second") Integer second) {
+
+        return "The division result is = " + Math.pow( first,second);
+
+    }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AssetStoreApplication.class, args);
     }
