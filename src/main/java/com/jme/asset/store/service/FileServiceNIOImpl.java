b3acabac697b11afc2c0da04e998d88fb3163d89
Denis <denislesheniuk@gmail.com>
2017-10-10 00:22:57 +0300

    Added new FileService implementation

---
+++ src/main/java/com/jme/asset/store/service/FileServiceNIOImpl.java
@@ -0,0 +1,45 @@
+package com.jme.asset.store.service;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.*;
+import java.util.Random;
+
+@Service("fileServiceNIO")
+public class FileServiceNIOImpl implements FileService {
+
+    @Override
+    public ResponseEntity<?> fileUpload(MultipartFile file) {
+        Path path = Paths.get("src/main/resources/public/" + file.getOriginalFilename());
+        try {
+            byte[] bytes = file.getBytes();
+            Path newFile = Files.write(path, bytes, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("IOException");
+        }
+        return ResponseEntity.ok().body("upload");
+    }
+
+    @Override
+    public ResponseEntity<?> randomFileGeneration() {
+        Random random = new Random(System.currentTimeMillis());
+        int arrSize = random.nextInt(1000);
+        byte[] bytes = new byte[arrSize];
+        for (int i = 0; i < arrSize; i++) {
+            bytes[i] = (byte) random.nextInt();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(bytes);
+    }
+}
