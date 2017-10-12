e78655d8192844ed322606d465da4e8c3f8d6b1c
Alex Brui <javasabr@gmail.com>
2017-10-12 14:46:06 +0300

    updated main class.

--- src/main/java/com/jme/asset/store/AssetStoreApplication.java
+++ src/main/java/com/jme/asset/store/AssetStoreApplication.java
@@ -1,27 +1,20 @@
 package com.jme.asset.store;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class AssetStoreApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-
-    @RequestMapping(value = "/math/add", method = RequestMethod.GET )
-    @ResponseBody
-    String add(@RequestParam("first") Integer first, @RequestParam("second") Integer second){
-        return "The sum is = " + (first + second);
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AssetStoreApplication.class, args);
+    }
+
+    @RequestMapping(value = "/home")
+    public String root() {
+        return "index";
     }
 }
