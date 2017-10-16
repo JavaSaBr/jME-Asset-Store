3f29f8dcbb5fb3b9020f95cc3979a59bbaf1bb31
YunkevichAndrei <andreyy.by@mail.ru>
2017-10-16 20:42:15 +0300

    change

--- src/main/java/com/jme/asset/store/AssetStoreApplication.java
+++ src/main/java/com/jme/asset/store/AssetStoreApplication.java
@@ -2,8 +2,6 @@ package com.jme.asset.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class AssetStoreApplication {
