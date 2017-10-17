c37d1da965002fbcc4cae50adedd9b9e66821824
Alex Brui <javasabr@gmail.com>
2017-10-17 09:58:49 +0300

    updated front end

--- front-end/src/app.module.ts
+++ front-end/src/app.module.ts
@@ -1,16 +1,35 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing/app-routing.module';
+import {MatButtonModule, MatInputModule, MatToolbarModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
+import {LoginComponent} from "./component/page/login/login.component";
+import {RegisterComponent} from "./component/page/register/register.component";
+import {DashboardComponent} from "./component/page/dashboard/dashboard.component";
+import {SecurityService} from "./service/security.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
   ],
-  providers: [],
+  providers: [SecurityService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
