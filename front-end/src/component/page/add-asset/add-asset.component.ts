3f70d389b50c8a2c71d83b8beb692556b62698ba
mishagrodno <mishagrodno@mail.ru>
2017-11-04 13:50:40 +0300

    started develop AssetService

---
+++ front-end/src/component/page/add-asset/add-asset.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-add-asset',
+  templateUrl: './add-asset.component.html',
+  styleUrls: ['./add-asset.component.css'],
+})
+
+export class AddAssetComponent{
+
+}
---
+++ front-end/src/service/asset/asset.service.ts
@@ -0,0 +1,9 @@
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class AssetService{
+
+  public static readonly ADD_ASSET_URL = "/assets/add/asset";
+
+  
+}
