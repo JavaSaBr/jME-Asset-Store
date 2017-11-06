5ba5222c3c6e4181fa6c137c064e5b0959215114
Denis <denislesheniuk@gmail.com>
2017-11-07 00:07:37 +0300

    Added asset-category.service template.

---
+++ front-end/src/service/asset-category.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AssetCategoryService {
+
+  private static readonly GET_CATEGORIES_URL = "";
+  private static readonly ADD_CATEGORY_URL = "";
+  private static readonly REMOVE_CATEGORY_URL = "";
+  private static readonly GET_CHILD_CATEGORIES = "";
+
+  constructor() { }
+
+  public getCategories(){
+
+  }
+
+  public addCategory(){
+
+  }
+
+  public removeCategory(categoryId: number){
+
+  }
+
+  public getChildCetegories(parentId: number){
+
+  }
+}
