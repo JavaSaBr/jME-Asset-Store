42315f3ed25e53e04cbf04dd7c5a4f7226de89f5
Denis <denislesheniuk@gmail.com>
2017-11-07 00:43:55 +0300

    Added implementation of asset category service's methods.

--- front-end/src/service/asset-category.service.ts
+++ front-end/src/service/asset-category.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AssetCategoryEntity} from "../model/entity/AssetCategoryEntity";
+import {Http} from "@angular/http";
+import {Utils} from "../util/utils";
+import {AssetCategoryParam} from "../model/category/AssetCategoryParam";
 
 /**
  * The asset category service.
@@ -13,21 +17,50 @@ export class AssetCategoryService {
   private static readonly REMOVE_CATEGORY_URL = "";
   private static readonly GET_CHILD_CATEGORIES = "";
 
-  constructor() { }
-
-  public getCategories(){
-
+  constructor(private readonly http: Http) {
   }
 
-  public addCategory(){
-
+  public getCategories(): Promise<AssetCategoryEntity[]> {
+    return this.http.get(AssetCategoryService.GET_CATEGORIES_URL)
+      .toPromise()
+      .then(responce => {
+        let body = responce.json();
+        return body;
+      })
+      .catch(this.handleError);
   }
 
-  public removeCategory(categoryId: number){
-
+  public addCategory(assetCategoryParam: AssetCategoryParam,
+                     handler: (message: string, result: boolean) => void) {
+    this.http.post(AssetCategoryService.ADD_CATEGORY_URL, assetCategoryParam)
+      .toPromise()
+      .then(response => handler(null, true))
+      .catch(error => Utils.handleErrorMessageJson(error, (ex: string) => handler(ex, false)));
   }
 
-  public getChildCetegories(parentId: number){
+  public removeCategory(parentID: string, handler: (message: string, result: boolean) => void) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('id', parentID);
+    this.http.get(AssetCategoryService.REMOVE_CATEGORY_URL, {search: params})
+      .toPromise()
+      .then(response => handler(null, true))
+      .catch(error => Utils.handleErrorMessageJson(error, (ex: string) => handler(ex, false)));
+  }
 
+  public getChildCetegories(parentId: string): Promise<AssetCategoryEntity[]> {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('id', parentId);
+    return this.http.get(AssetCategoryService.GET_CHILD_CATEGORIES, {search: params})
+      .toPromise()
+      .then(responce => {
+        let body = responce.json();
+        return body;
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: Response | any) {
+    console.error(error.message || error);
+    return Promise.reject(error.message || error);
   }
 }
