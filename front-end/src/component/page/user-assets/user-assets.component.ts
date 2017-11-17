ad727a49d3f4cbb1ebd15efd6532549fddae11c8
Denis <denislesheniuk@gmail.com>
2017-11-17 16:09:30 +0300

    Added table

--- front-end/src/component/page/user-assets/user-assets.component.ts
+++ front-end/src/component/page/user-assets/user-assets.component.ts
@@ -4,37 +4,30 @@ import {Router} from '@angular/router';
 import {PageComponent} from "../../page.component";
 import {AssetEntity} from "../../../model/entity/asset-entity";
 import {UserService} from "../../../service/user-service";
+import {DataSource} from "@angular/cdk/collections";
+import {AssetDataSource} from "../../../service/asset/asset-data-source";
 
 @Component({
   moduleId: module.id,
   selector: 'app-user-assets-asset',
   templateUrl: './user-assets.component.html',
-  providers: [AssetService, UserService],
+  providers: [AssetService, UserService, AssetDataSource],
   styleUrls: ['./user-assets.component.css']
 })
 
 export class UserAssetsComponent extends PageComponent {
 
-  constructor(private assetService: AssetService, private router: Router) {
+  private _error: string;
+  private displayedColumns: string[] = ['Name', 'Description'];
+  private dataSource: DataSource<AssetEntity>;
+
+  constructor(private router: Router, private readonly assetDataSource: AssetDataSource) {
     super();
+    this.dataSource = assetDataSource;
   }
 
   ngOnInit() {
-    this.tryGetUsersAssets();
-  }
-
-  private _assets: AssetEntity[];
-  private _error: string;
-
-  tryGetUsersAssets(): void {
-    this.assetService.loadAssets((message, assets) => {
-      if (message == null) {
-        this.assets = assets;
-      }
-      else {
-        this._error = message;
-      }
-    })
+    this.assetDataSource.refresh();
   }
 
   createAsset() {
@@ -45,14 +38,6 @@ export class UserAssetsComponent extends PageComponent {
     this.router.navigate(['/assets', id]);
   }
 
-  get assets() {
-    return this._assets;
-  }
-
-  set assets(assets: AssetEntity[]) {
-    this._assets = assets;
-  }
-
   get error() {
     return this._error;
   }
@@ -60,4 +45,6 @@ export class UserAssetsComponent extends PageComponent {
   set error(error: string) {
     this._error = error;
   }
+
+
 }
